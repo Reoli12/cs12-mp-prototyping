5db3feb6f20fa29d1c1bc28ffe40a3e3aab30606
reoli <domondongeron2@gmail.com>
2025-05-25 22:59:17 +0700

    0a done, egg class defined

---
+++ imp1/phase00/project_types.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass
+class Egg:
+    height: int
+    width: int
+    total_hp: int
+    current_hp: int
+    
--- imp1/phase00/prot.py
+++ imp1/phase00/prot.py
@@ -1,7 +1,21 @@
 import pyxel
+from project_types import Egg
+
+SCREEN_WIDTH = 300
+SCREEN_HEIGHT = 300
+FPS = 30
+
+EGG_HEIGHT = 20
+EGG_WID = 10
+EGG_HP = 10
+
+EGG = Egg(EGG_HEIGHT, EGG_WID, EGG_HP, EGG_HP)
 
 def update():
     ...
 
 def draw():
     ...
+
+pyxel.init(SCREEN_HEIGHT, SCREEN_WIDTH)
+pyxel.run(update, draw)
