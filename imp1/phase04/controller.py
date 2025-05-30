314dc369626e55f0b50aef03826feaf49748e092
jpayawal <jmpayawal@up.edu.ph>
2025-05-30 20:47:52 +0800

    phase04 directory

---
+++ imp1/phase04/controller.py
@@ -0,0 +1,179 @@
+from model import Model
+from view import View
+from copy import deepcopy
+
+
+class Controller:
+    def __init__(self, model: Model, view: View):
+        self._model: Model = model
+        self._view: View = view
+
+    def update(self):
+        if self._model.is_game_over or self._model.is_game_won:
+            if not self._model.is_time_get and self._model.is_game_won:
+                min = deepcopy(self._model.min)
+                sec = deepcopy(self._model.sec)
+                self._model.update_leaderboards(min, sec)
+            if self._view.is_restart_pressed():
+                self._model.restart()
+            return
+
+        self._model.update(
+            self._view.is_forward_pressed(), 
+            self._view.is_left_pressed(), 
+            self._view.is_down_pressed(), 
+            self._view.is_right_pressed(),
+            self._view.is_attack_pressed()
+            )
+
+    def draw(self):
+        self._view.clear_screen()
+
+        #player and camera
+        player_x_pos: int = int(self._model.player_egg.leftmost_point)
+        player_y_pos: int = int(self._model.player_egg.topmost_point)
+        player_width: int = self._model.player_egg.stats.width
+        player_height: int = self._model.player_egg.stats.height
+        
+        camera_x_pos: int = player_x_pos - self._model.screen_width // 2
+        camera_y_pos: int = player_y_pos - self._model.screen_height // 2 + 5
+
+        player_cur_hp: int = self._model.player_egg.stats.current_hp
+        player_max_hp: int = self._model.player_egg.stats.max_hp
+        player_hp_x_pos: int = int((player_x_pos - player_width / 2))
+        player_hp_y_pos: int = int(player_y_pos + int(player_height * 1.25))
+
+        #player egg
+        if not self._model.is_game_over:
+            self._view.draw_egg(
+                player_x_pos - camera_x_pos, 
+                player_y_pos - camera_y_pos, 
+                player_width, 
+                player_height)
+        
+        #player hp
+        self._view.text_player_health(
+            player_hp_x_pos - camera_x_pos, 
+            player_hp_y_pos - camera_y_pos, 
+            player_cur_hp, 
+            player_max_hp)
+
+        #eggnemies
+        eggnemies = self._model.eggnemies
+        for eggnemy in eggnemies:
+            eggnemy_x_pos: int = int(eggnemy.leftmost_point)
+            eggnemy_y_pos: int = int(eggnemy.topmost_point)
+            eggnemy_width: int = eggnemy.stats.width
+            eggnemy_height: int = eggnemy.stats.height
+
+            eggnemy_cur_hp: int = eggnemy.stats.current_hp
+            eggnemy_max_hp: int = eggnemy.stats.max_hp
+            eggnemy_hp_x: int = int((eggnemy_x_pos -( eggnemy_width / 2) ** 0.5))
+            eggnemy_hp_y: int = int(eggnemy_y_pos + int(eggnemy_height * 1.5))
+
+            #eggnemy
+            self._view.draw_eggnemy(
+                eggnemy_x_pos - camera_x_pos, 
+                eggnemy_y_pos - camera_y_pos, 
+                eggnemy_width, 
+                eggnemy_height)
+            
+            #eggnemy hp
+            self._view.text_eggnemy_health(
+                eggnemy_hp_x - camera_x_pos, 
+                eggnemy_hp_y - camera_y_pos, 
+                eggnemy_cur_hp, 
+                eggnemy_max_hp)
+            
+        #boss
+        boss = self._model.boss_egg
+        if boss:
+            assert boss
+            boss_x_pos: int = int(boss.leftmost_point)
+            boss_y_pos: int = int(boss.topmost_point)
+            boss_width: int = boss.stats.width
+            boss_height: int = boss.stats.height
+
+            boss_cur_hp: int = boss.stats.current_hp
+            boss_max_hp: int = boss.stats.max_hp
+            boss_hp_x: int = int((boss_x_pos - (boss_width / 2) ** 0.5))
+            boss_hp_y: int = int(boss_y_pos + int(boss_height * 1.25))
+
+            #boss
+            self._view.draw_boss(
+                boss_x_pos - camera_x_pos, 
+                boss_y_pos - camera_y_pos, 
+                boss_width, 
+                boss_height)
+
+            #boss hp
+            self._view.text_boss_health(
+                boss_hp_x - camera_x_pos, 
+                boss_hp_y - camera_y_pos, 
+                boss_cur_hp, 
+                boss_max_hp)
+            
+        #world border
+        self._view.draw_world(-camera_x_pos, -camera_y_pos)
+
+        #stats
+        num_defeated_eggnemies: int = self._model.num_defeated_eggnemies
+        num_defeated_x_pos: int = 7
+        num_defeated_y_pos: int = 7
+
+        #num defeated
+        self._view.text_num_defeated_eggnemy(
+            num_defeated_x_pos, 
+            num_defeated_y_pos, 
+            num_defeated_eggnemies)
+        
+        time_x_pos: int = self._model.screen_width - 25
+        time_y_pos: int = 7
+        sec: int = self._model.sec
+        sec_str: str = f'{sec}' if sec > 9 else f'0{sec}'
+        min: int = self._model.min
+        min_str: str = f'{min}' if min > 9 else f'0{min}'
+        time: str = f'{min_str}:{sec_str}'
+
+        #time
+        self._view.text_time(
+            time_x_pos, 
+            time_y_pos,
+            time)
+
+        #leaderboards
+        if self._model.is_game_over or self._model.is_game_won:
+            runs_str: list[str] = self._model.leaderboards_str
+
+            leaderboard_x_pos = 7
+            spacing = 10
+            leaderboard_y_pos = self._model.screen_height - (spacing * len(runs_str)) - 7
+            
+            for i, top_runs_str in enumerate(runs_str):
+                self._view.text_leaderboards(
+                    leaderboard_x_pos,
+                    leaderboard_y_pos + (i * spacing),
+                    top_runs_str)
+        
+        #end state messages
+        if self._model.is_game_won:
+            win_x_pos: int = int((player_x_pos - int((player_width / 2) * 2)))
+            win_y_pos: int = int(player_y_pos - int(player_height))
+
+            #win message
+            self._view.text_win_message(
+            win_x_pos - camera_x_pos, 
+            win_y_pos - camera_y_pos)
+
+        if self._model.is_game_over or self._model.is_game_won:
+            restart_x_pos = player_hp_x_pos - 13
+            restart_y_pos = int(player_y_pos + int(player_height * 2))
+
+            #restart message
+            self._view.text_restart_message(
+                restart_x_pos - camera_x_pos,
+                restart_y_pos - camera_y_pos
+            )
+        
+    def start(self):
+        self._view.start(self._model.fps, self, self)
---
+++ imp1/phase04/egg_rally.py
@@ -0,0 +1,85 @@
+from model import Model
+from view import View
+from controller import Controller
+from project_types import Point, PlayerEgg, EggInfo, GameSettings
+import json
+
+with open('settings.json', 'r') as file:
+    data = json.load(file)
+
+def main():
+    #Game Settings
+    fps: int = data["fps"]
+    world_width: int = data["worldWidth"]
+    world_height: int = data["worldHeight"]
+    screen_width: int = data["screenWidth"]
+    screen_height: int = data["screenHeight"]
+
+    settings = GameSettings(
+        fps,
+        world_width,
+        world_height,
+        screen_width,
+        screen_height
+    )
+
+    #Player Info
+    egg_hp: int = data["playerEggHp"]
+    egg_width: int = data["playerEggWidth"]
+    egg_height: int = data["playerEggHeight"]
+    egg_speed: int = data["playerEggSpeed"]
+    egg_damage: int = data["playerAttackDamage"]
+    egg_attack_radius: int = data["playerAttackRadius"]
+
+    player_egg = PlayerEgg(
+        EggInfo(
+            egg_width,
+            egg_height,
+            egg_hp,
+            egg_hp,
+            egg_speed
+        ),
+        Point(world_width / 2, world_height / 2),
+        egg_damage,
+        egg_attack_radius
+    )
+
+    #eggnemy Info
+    eggnemy_hp: int = data["eggnemInitialHp"]
+    eggnemy_count: int = data["eggnemyInitialCount"]
+    eggnemy_width: int = data["eggnemyWidth"]
+    eggnemy_height: int = data["eggnemyHeight"]
+    eggnemy_speed: int = data["eggnemySpeed"]
+
+    eggnemy_info = EggInfo(
+        eggnemy_width,
+        eggnemy_height,
+        eggnemy_hp,
+        eggnemy_hp,
+        eggnemy_speed
+    )
+
+    #boss Info
+    boss_spawn_rate: int = data["bossSpawnRate"]
+    boss_hp: int = data["bossInitialHp"]
+    boss_width: int = data["bossWidth"]
+    boss_height: int = data["bossHeight"]
+    boss_speed: int = data["bossSpeed"]
+
+    boss_info = EggInfo(
+        boss_width,
+        boss_height,
+        boss_hp,
+        boss_hp,
+        boss_speed
+    )
+
+    #MVC init
+    model = Model(player_egg, settings, eggnemy_count, eggnemy_info, boss_info, boss_spawn_rate)
+    view = View(settings)
+    controller = Controller(model, view)
+
+    controller.start()
+
+if __name__ == '__main__':
+    main()
