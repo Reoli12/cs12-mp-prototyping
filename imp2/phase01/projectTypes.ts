7e405350f9ea2ea9ab77a008a7e39aff28c52625
reoli <domondongeron2@gmail.com>
2025-05-30 21:43:44 +0700

    added screenHeight and screenWidth, boundary is now a hollow white square

---
+++ imp2/phase01/projectTypes.ts
@@ -0,0 +1,76 @@
+import { Schema as S } from 'effect'
+
+export const EggSides = S.Union(
+    S.Literal("top"),
+    S.Literal("bottom"),
+    S.Literal("left"),
+    S.Literal("right"),
+)
+export type EggSides = typeof EggSides.Type
+
+export const Point = S.Struct({
+    x: S.Number,
+    y: S.Number,
+})
+export type Point= typeof Point.Type
+
+export const PlayerEgg = S.TaggedStruct("PlayerEgg", {
+    centerCoords: Point,
+    height: S.Number,
+    width: S.Number,
+    total_hp: S.Number,
+    current_hp: S.Number,
+    color: S.String,
+    speed: S.Number,
+    attackRange: S.Number,
+    frameCountSinceLastDamaged: S.Option(S.Number),
+})
+export type PlayerEgg = typeof PlayerEgg.Type
+
+export const Eggnemy = S.TaggedStruct("Eggnemy", {
+    // yes, redundant but it may be more helpful to 
+    // distinguish between the two later down the line
+    centerCoords: Point,
+    height: S.Number,
+    width: S.Number,
+    color: S.String,
+    speed: S.Number
+})
+export type Eggnemy = typeof Eggnemy.Type
+
+export const Egg = S.Union(
+    PlayerEgg,
+    Eggnemy,
+)
+export type Egg = typeof Egg.Type
+
+export const Model = S.Struct({
+    playerEgg: PlayerEgg,
+    eggnemies: S.Array(Eggnemy),
+    fps: S.Int,
+    currentFrame: S.Int,
+    worldHeight: S.Number,
+    worldWidth: S.Number,
+    screenHeight: S.Number,
+    screenWidth: S.Number,
+    isOver: S.Boolean,
+    })
+export type Model = typeof Model.Type
+
+const Settings = S.Struct({
+    fps: S.Number,
+    screenWidth: S.Number,
+    screenHeight: S.Number,
+    worldWidth: S.Number,
+    worldHeight: S.Number,
+    playerEggHp: S.Number,
+    playerEggWidth: S.Number,
+    playerEggHeight: S.Number,
+    playerEggRange: S.Number,
+    eggnemyCount: S.Number,
+    eggnemyWidth: S.Number,
+    eggnemyHeight: S.Number,
+    playerEggSpeed: S.Number,
+    eggnemySpeed: S.Number,
+})
+export type Settings = typeof Settings.Type
