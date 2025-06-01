5ded845848af6a5f868fcc2a1a73014058104170
reoli <domondongeron2@gmail.com>
2025-06-01 13:00:22 +0700

    restart implemented

---
+++ imp2/phase03/model.ts
@@ -0,0 +1,94 @@
+import { Array, Option, Schema as S } from 'effect'
+import {
+    PlayerEgg,
+    Eggnemy,
+    Boss,
+    Point,
+    minsSecs,
+    settings
+} from "./projectTypes"
+
+export const Model = S.Struct({
+    playerEgg: PlayerEgg,
+    eggnemies: S.Array(Eggnemy),
+    eggnemiesDefeated: S.Number,
+    eggnemiesToKillBeforeBoss: S.Number,
+    bosses: S.Array(Boss),
+    fps: S.Int,
+    isBossActive: S.Boolean,
+    currentFrame: S.Int,
+    currentTime: minsSecs,
+    worldHeight: S.Number,
+    worldWidth: S.Number,
+    worldCenter: Point,
+    worldBoundaryWidth: S.Number,
+    screenHeight: S.Number,
+    screenWidth: S.Number,
+    gameState: S.Union(
+        S.Literal("Ongoing"),
+        S.Literal("PlayerWin"),
+        S.Literal("PlayerLose")
+    ),
+    })
+export type Model = typeof Model.Type
+
+export const playerEgg = PlayerEgg.make({
+        centerCoords: Point.make({
+            x: settings.screenWidth / 2,
+            y: settings.screenHeight / 2,
+        }),
+        height: 20,
+        width: 10,
+        totalHp: 20,
+        currentHp: 20,
+        color: "white",
+        speed: settings.playerEggSpeed,
+        attackRange: settings.playerEggRange,
+        frameCountSinceLastDamaged: Option.none(),
+        damage: 3,
+    })
+
+export const initModel = Model.make({
+        playerEgg: playerEgg,
+        eggnemies: generateInitialEggnemies(settings.initialEggnemyCount),
+        bosses: Array.empty(),
+        isBossActive: false,
+        eggnemiesDefeated: 0,
+        eggnemiesToKillBeforeBoss: settings.eggnemiesToKillBeforeBoss,
+        worldHeight: settings.worldHeight,
+        worldWidth: settings.worldWidth,
+        worldCenter: Point.make({
+            x: settings.screenWidth / 2,
+            y: settings.screenHeight / 2,
+        }),
+        worldBoundaryWidth: settings.worldBoundaryWidth,
+        screenHeight: settings.screenHeight,
+        screenWidth: settings.screenWidth,
+        fps: settings.fps,
+        currentFrame: 0,
+        currentTime: minsSecs.make({
+            mins: 0,
+            secs: 0,
+        }),
+        gameState: "Ongoing",
+    })
+
+function generateInitialEggnemies(num: number): Eggnemy[] {
+    let ret: Eggnemy[] = Array.empty()
+    for (let i = 0; i < num; i++) {
+        ret = Array.append(ret, Eggnemy.make({
+            centerCoords: Point.make({
+                x: Math.random() * settings.screenWidth,
+                y: Math.random() * settings.screenHeight
+            }),
+            height: settings.eggnemyHeight,
+            width: settings.eggnemyWidth,
+            color: "gray",
+            speed: settings.eggnemySpeed,
+            damage: 1,
+            currentHp: settings.eggnemyInitialHp,
+            totalHp: settings.eggnemyInitialHp,
+        }))
+    }
+    return ret
+}
